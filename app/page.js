652a4d1d3f60fdace7eb4c33691e626cadbc53dd
hafij <hafij.alasad23@gmail.com>
2025-01-26 23:18:19 +0600

    refactor: title text size decreased

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import ObjectDetectionFromImage from "@/components/object-detection-from-image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
-      <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
+      <h1 className="gradient-title font-extrabold text-2xl md:text-4xl lg:text-6xl tracking-tighter md:px-6 text-center">
         Object Detection from Image
       </h1>
       {/* <ObjectDetection /> */}
